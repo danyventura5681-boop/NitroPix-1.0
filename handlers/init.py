e4562056b1c4ff2f01e32a55c369664445923b94
danyventura5681-boop <danyventura5681@gmail.com>
2026-03-19 14:31:48 -0400

    Actualizar init.py

--- handlers/init.py
+++ handlers/init.py
@@ -1,1 +1,37 @@
-# This file makes handlers a Python package
+from telegram.ext import Application
+
+# importar módulos
+from . import start
+from . import effects
+from . import admin
+from . import daily
+from . import referral
+from . import recharge
+from . import process
+
+
+def register_handlers(app: Application):
+    """
+    Registra TODOS los handlers del bot
+    """
+
+    # START
+    app.add_handler(start.get_handlers())
+
+    # EFFECTS
+    app.add_handler(effects.get_handlers())
+
+    # ADMIN
+    app.add_handler(admin.get_handlers())
+
+    # DAILY
+    app.add_handler(daily.get_handlers())
+
+    # REFERRAL
+    app.add_handler(referral.get_handlers())
+
+    # RECHARGE
+    app.add_handler(recharge.get_handlers())
+
+    # PROCESS (photos / IA)
+    app.add_handler(process.get_handlers())
